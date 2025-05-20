c65e88568ded5b79777e9eab398ae36e2a5a8161
NicolasBaldo <septfoisept@outlook.fr>
2025-05-20 10:42:20 +0200

    Css1

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,22 +9,17 @@ function Home() {
     navigate('/other');
   };
 
-  return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      backgroundColor: '#f0f0f0'
-    }}>
-      <img
-        src="rose-essai.jpg" // Mets ton image dans le dossier `public`
-        alt="Go"
-        onClick={handleClick}
-        style={{ cursor: 'pointer', width: '300px', height: 'auto' }}
-      />
-    </div>
-  );
+return (
+  <div className="image-container">
+    <img
+      src="rose-essai.jpg"
+      alt="Go"
+      onClick={handleClick}
+      className="clickable-image"
+    />
+  </div>
+);
+
 }
 
 export default Home;
